4467f8fa4b0e914a059f3960c51951d3a1708349
Fox <fox@MacBook-Air-Fox.local>
2024-12-14 23:05:26 +0500

    i added users and login page and his functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,62 @@
 import 'react'
 import './App.css'
 import { getProducts } from './requests/requests'
+import { Input , Button, Form} from "antd";
+import { useForm , SubmitHandler, Controller} from 'react-hook-form';
+import { useState } from 'react';
+
 
 function App() {
-    const result = getProducts
-    console.log(result)
+    getProducts()
+    const result = JSON.parse(sessionStorage.getItem("data"))
+    interface UserInf {
+        login?: string;
+        password?: string|number;
+    }
+    const {control, handleSubmit, reset, formState: { errors }} = useForm<UserInf>();
+    const onSubmit: SubmitHandler<UserInf> = (data) => {
+        console.log(data);
+        reset();
+      };
   return (
     <>
+    <Form onSubmitCapture={handleSubmit(onSubmit)}>
+    <Controller
+          name="login"
+          control={control}  // react-hook-form ning control obyektini uzatamiz
+          defaultValue=""  // boshlang'ich qiymat
+          rules={{ required: "Bu maydon majburiy",
+            validate: value => value === result[0].username || "User notogri kiritildi qayta urinib koring"
+          }}  // Validatsiya
+          render={({ field }) => (
+            <Input
+            {...field}  // react-hook-formdan kerakli fieldni uzatish
+            placeholder="Foydalanuvchi nomini kiriting"
+            style={{ width: "100%" }}
+          />
+          )}
+          />
+          {errors.login && <p style={{ color: "red" }}>{errors.login.message}</p>}
+    <Controller
+          name="password"
+          control={control}  // react-hook-form ning control obyektini uzatamiz
+          defaultValue=""  // boshlang'ich qiymat
+          rules={{ required: "Bu maydon majburiy",
+            validate: value => value === result[0].username || "Parol notogri kiritildi qayta urinib koring"
+           }}  // Validatsiya
+          render={({ field }) => (
+            <Input
+            {...field}  // react-hook-formdan kerakli fieldni uzatish
+            placeholder="Foydalanuvchi parolini kiriting"
+            style={{ width: "100%" }}
+          />
+          )}
+          />
+          {errors.password && <p style={{ color: "red" }}>{errors.password.message}</p>}
+    <button>Log In</button>
+    </Form>
+    <br />
+    <p>Don't have an account yet?<Button type='link'>Sign In</Button></p>
 
     </>
   )
--- src/requests/requests.tsx
+++ src/requests/requests.tsx
@@ -1,5 +1,8 @@
-
 import api from './api'
 
-export const getProducts = api('users', 'GET', )
+export const getProducts = async()=>{
+    const response = await api('users', 'GET')
+    sessionStorage.setItem("data", JSON.stringify(response.data))
+}
+
 export const addProduct = (product:object) => api( 'users', 'POST' , product)
